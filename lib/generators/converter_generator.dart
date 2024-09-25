1a18c6b18aaf07411af605ef42bb6200f7788435
Kevin Tan (valius) <kevin.tan4@gmail.com>
2024-09-24 18:15:56 -0700

    feat: add SSC conversion support

--- lib/generators/converter_generator.dart
+++ lib/generators/converter_generator.dart
@@ -2,12 +2,15 @@ import "package:ucsconvertertool/converters/fallback_converter.dart";
 import "package:ucsconvertertool/converters/i_converter.dart";
 import "package:path/path.dart" as p;
 import "package:ucsconvertertool/converters/sm_converter.dart";
+import "package:ucsconvertertool/converters/ssc_converter.dart";
 
 class ConverterGenerator {
   static IConverter createConverter(String filename) {
     String ext = p.extension(filename);
     if (ext.toUpperCase() == ".SM") {
       return SMConverter(filename);
+    } else if (ext.toUpperCase() == ".SSC") {
+      return SSCConverter(filename);
     } else {
       return FallbackConverter(filename);
     }
